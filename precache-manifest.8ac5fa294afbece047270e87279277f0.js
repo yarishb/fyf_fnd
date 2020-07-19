f65cc447051f8568e5e99faea29ddad34eb25030
yarishb <bogdan040105@gmail.com>
2020-07-19 14:03:06 +0300

    Updates

---
+++ precache-manifest.8ac5fa294afbece047270e87279277f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04122e8ac74419b066c27bcfebc90924",
+    "url": "/fyf_fnd/index.html"
+  },
+  {
+    "revision": "6ad31a31491959177581",
+    "url": "/fyf_fnd/static/css/main.05982248.chunk.css"
+  },
+  {
+    "revision": "b6008b8dcf16163e1a36",
+    "url": "/fyf_fnd/static/js/2.6728270e.chunk.js"
+  },
+  {
+    "revision": "2d70f05d135acc7c7cd2dabe17002c00",
+    "url": "/fyf_fnd/static/js/2.6728270e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad31a31491959177581",
+    "url": "/fyf_fnd/static/js/main.f7549913.chunk.js"
+  },
+  {
+    "revision": "c19707419099f926645a",
+    "url": "/fyf_fnd/static/js/runtime-main.7697d621.js"
+  }
+]);
